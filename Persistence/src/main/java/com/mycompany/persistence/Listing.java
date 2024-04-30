37aabb0f6cca657f87bac4fba769e1ba04cc2b9e
bryan943 <santiagomotta350@gmail.com>
2024-04-29 23:42:30 -0500

    Authors names

--- Persistence/src/main/java/com/mycompany/persistence/Listing.java
+++ Persistence/src/main/java/com/mycompany/persistence/Listing.java
@@ -12,7 +12,7 @@ import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author Brayan Santiago Ramirez Silva
- *         Miguel Pacheco
+ *         Miguel Jose Vargas Pacheco
  */
 public class Listing {
 
